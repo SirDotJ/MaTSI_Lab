df7661cd641b2f544b863e5995022e0b5ac58eb4
SirDotJ_Honor <sirdotj@gmail.com>
2023-11-22 15:23:35 +1000

    Рабочее решение криптосистемы Эль-Гамаля, готовка к реструктурированию и внесению в GUI. Небольшой рефакторинг RSA для визуального представления примера.

---
+++ src/main/java/org/example/lab8/Tritemius.java
@@ -0,0 +1,11 @@
+package org.example.lab8;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Tritemius {
+//    public static List<Integer> input = new ArrayList<>(Arrays.asList(
+//
+//    ))
+}
